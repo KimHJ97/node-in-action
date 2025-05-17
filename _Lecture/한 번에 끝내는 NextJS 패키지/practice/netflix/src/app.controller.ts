d67f33661dcebbe0bb1a0ee39ea6be3f895d7652
로그 <hj_kim97@naver.com>
2025-05-17 14:14:55 +0900

    Service 생성

--- _Lecture/한 번에 끝내는 NextJS 패키지/practice/netflix/src/app.controller.ts
+++ _Lecture/한 번에 끝내는 NextJS 패키지/practice/netflix/src/app.controller.ts
@@ -3,7 +3,6 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Patch,
   Post,
@@ -11,83 +10,32 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 
-interface Movie {
-  id: number;
-  title: string;
-}
-
 @Controller('/movie')
 export class AppController {
-  private movies: Movie[] = [
-    {
-      id: 1,
-      title: '해리포터',
-    },
-    {
-      id: 2,
-      title: '반지의 제왕',
-    },
-  ];
-  private idCounter = 3;
-
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getMovies(@Query('title') title: string) {
-    if (!title) {
-      return this.movies;
-    }
-
-    return this.movies.filter((m) => m.title.startsWith(title));
+    return this.appService.getManyMovies(title);
   }
 
   @Get('/:id')
   getMovie(@Param('id') id: string) {
-    const movie = this.movies.find((m) => m.id === +id);
-
-    if (!movie) {
-      //throw new Error('movie not found');
-      throw new NotFoundException('movie not found');
-    }
-
-    return movie;
+    return this.appService.getMovieById(+id);
   }
 
   @Post()
   postMovie(@Body('title') title: string) {
-    const movie: Movie = {
-      id: this.idCounter,
-      title: title,
-    };
-
-    this.movies.push(movie);
-
-    return movie;
+    return this.appService.createMovie(title);
   }
 
   @Patch('/:id')
   patchMovie(@Param('id') id: string, @Body('title') title: string) {
-    const movie = this.movies.find((m) => m.id === +id);
-
-    if (!movie) {
-      throw new NotFoundException('movie not found');
-    }
-
-    Object.assign(movie, { title });
-
-    return movie;
+    return this.appService.updateMovie(+id, title);
   }
 
   @Delete('/:id')
   deleteMovie(@Param('id') id: string) {
-    const movieIndex = this.movies.findIndex((m) => m.id === +id);
-
-    if (movieIndex === -1) {
-      throw new NotFoundException('movie not found');
-    }
-
-    this.movies.splice(movieIndex, 1);
-
-    return id;
+    return this.appService.deleteMovie(+id);
   }
 }
